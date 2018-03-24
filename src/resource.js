d1de414bc54a107253acdbf9563fdf3a78ee9fdf
mtti <mtti@users.noreply.github.com>
2018-03-24 16:16:10 +0200

    Upgrade to @mtti/nats-rest 0.6.0

--- src/resource.js
+++ src/resource.js
@@ -16,8 +16,7 @@ limitations under the License.
 
 const _ = require('lodash');
 const createError = require('http-errors');
-const jsonpatch = require('fast-json-patch');
-const { ResourceServer, InstanceAction } = require('@mtti/nats-rest');
+const { ResourceServer } = require('@mtti/nats-rest');
 
 class SequelizeResource {
   /**
@@ -46,39 +45,11 @@ class SequelizeResource {
     this._natsClient = natsClient;
     this._model = model;
     this._name = options.name || model.name;
+    this._logger = options.logger;
 
-    if (options.logger) {
-      this._logger = options.logger;
-    }
-
-    this._defaultBodySchema = this._model.defaultBodySchema;
     this._jsonSchemas = this._model.jsonSchemas || {};
+    this._schemaRef = this._model.schemaRef;
     this._actions = [];
-
-    if (options.defaultActions !== false) {
-      if (!this._defaultBodySchema) {
-        throw new Error('defaultBodySchema is required with defaultActions');
-      }
-
-      const getAction = new InstanceAction('GET', this._get.bind(this));
-      const putAction = new InstanceAction('PUT', this._put.bind(this))
-        .setLoadInstance(false)
-        .setBodySchema(this._defaultBodySchema);
-      const patchAction = new InstanceAction('PATCH', this._patch.bind(this))
-        .setBodySchema(false);
-      const postAction = new InstanceAction('POST', this._post.bind(this))
-        .setBodySchema(this._defaultBodySchema);
-      const deleteAction = new InstanceAction('DELETE', this._delete.bind(this))
-        .setLoadInstance(false);
-
-      Array.prototype.push.apply(this._actions, [
-        getAction,
-        putAction,
-        patchAction,
-        postAction,
-        deleteAction,
-      ]);
-    }
 
     if (this._model.actions) {
       Array.prototype.push.apply(this._actions, this._model.actions);
@@ -86,9 +57,17 @@ class SequelizeResource {
   }
 
   start() {
+    const adapter = {
+      load: this._load.bind(this),
+      toJSON: this._toJSON.bind(this),
+      upsert: this._upsert.bind(this),
+      delete: this._delete.bind(this),
+    };
+
     const serverOptions = {
-      instanceLoader: this._instanceLoader.bind(this),
+      adapter,
       jsonSchemas: this._jsonSchemas,
+      schemaRef: this._schemaRef,
       actions: this._actions,
       logger: this._logger,
     };
@@ -97,14 +76,11 @@ class SequelizeResource {
     this._model.hook('afterSave', (instance, options) => {
       this._server.emit(instance.toJSON());
     });
-    this._model.hook('afterDestroy', (instance, options) => {
-
-    });
 
     this._server.start();
   }
 
-  _instanceLoader(id) {
+  _load(id) {
     return this._model.findById(id)
       .then((instance) => {
         if (!instance) {
@@ -114,41 +90,19 @@ class SequelizeResource {
       });
   }
 
-  _get(instance) {
+  _toJSON(instance) {
     return instance.toJSON();
   }
 
-  _put(id, body) {
-    let bodyCopy = {};
-    if (body) {
-      bodyCopy = _.cloneDeep(body);
-    }
-
-    if (id) {
-      bodyCopy.id = id;
-    }
-
-    const instance = this._model.build(bodyCopy);
-    return instance.save();
-  }
-
-  _post(instance, body) {
-    return instance.update(body);
-  }
-
-  _patch(instance, patch) {
-    const instanceJSON = instance.toJSON();
-    const newBody = jsonpatch.applyPatch(instanceJSON, patch).newDocument;
-    delete newBody.id;
-    return instance.update(newBody);
+  _upsert(id, body) {
+    const bodyCopy = _.cloneDeep(body);
+    bodyCopy.id = id;
+    return this._model.upsert(bodyCopy)
+      .then(() => this._model.findById(id));
   }
 
   _delete(id) {
-    if (!id) {
-      return Promise.reject(createError(405));
-    }
-    return this._model.destroy({ where: { id }})
-      .then(() => ({ result: 'OK' }));
+    return this._model.destroy({ where: { id }});
   }
 }
 
